01ec73412000843e8276ef96204de304b9b96a91
nikivas <nikivas97@mail.ru>
2018-02-07 11:58:04 +0300

    with threadpool

--- Kontur.ImageTransformer/AsyncHttpServer.cs
+++ Kontur.ImageTransformer/AsyncHttpServer.cs
@@ -5,12 +5,15 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Text.RegularExpressions;
+using System.Collections.Concurrent;
+
 namespace Kontur.ImageTransformer
 {
     internal class AsyncHttpServer : IDisposable
     {
         public AsyncHttpServer()
         {
+            avalibleConnection = 0;
             listener = new HttpListener();
         }
         
@@ -66,14 +69,30 @@ namespace Kontur.ImageTransformer
         
         private void Listen()
         {
+            //ThreadPool.SetMinThreads(4, 4);
+            
             while (true)
             {
                 try
                 {
-                    if (listener.IsListening)
+                    if (listener.IsListening )
                     {
+                        //ConcurrentStack<HttpListenerContext> el = new ConcurrentStack<HttpListenerContext>();
                         var context = listener.GetContext();
-                        Task.Run(() => HandleContextAsync(context));
+                        //Console.WriteLine(avalibleConnection);
+                        if (avalibleConnection > 150)
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                            context.Response.Close();
+                            continue;
+                        }
+                        else
+                        {
+                            avalibleConnection++;
+                            //ThreadPool.QueueUserWorkItem(HandleContextAsync, context);
+                            Task.Run(() => HandleContextAsync(context));
+                        }
+
                     }
                     else Thread.Sleep(0);
                 }
@@ -98,20 +117,59 @@ namespace Kontur.ImageTransformer
 
             if(url[2] == "grayscale")
                 return new FilterGrayScale();
+
             else if (url[2] == "sepia")
                 return new FilterSepia();
+
             else if (Regex.IsMatch(url[2], "^threshold/(/d*/)$"))
                 return new FilterThreshold();
 
             return null;
         }
 
+        private void TestHandleContextAsync(object arg)
+        {
+            var canceller = new CancellationTokenSource();
+            canceller.CancelAfter(1000);
+            var listenerContext = (HttpListenerContext)arg;
+            var streamIn = listenerContext.Request.InputStream;
+            var encoding = listenerContext.Request.ContentEncoding;
+            var reader = new StreamReader(streamIn, encoding);
+            listenerContext.Response.StatusCode = (int)HttpStatusCode.OK;
+            
+            Thread.Sleep(90);
+            var res = reader.ReadToEnd();
+            int a;
+            int b;
+            ThreadPool.GetAvailableThreads(out a, out b);
+            //Console.WriteLine("workerThread - {0}, competitionPort - {1}",a,b);
+
+            using (var writer = new StreamWriter(listenerContext.Response.OutputStream, encoding))
+            {
+                writer.Write(res);
+            }
+            avalibleConnection--;
+        }
+
+        private void BadRequest(object arg)
+        {
+            var listenerContext = (HttpListenerContext) arg;
+            listenerContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            using (var writer = new StreamWriter(listenerContext.Response.OutputStream))
+            {
+                writer.Write(" ");
+            }
+        }
+
         private async Task HandleContextAsync(HttpListenerContext listenerContext)
         {
 
             // TODO: implement request handling
-            var url = listenerContext.Request.RawUrl.Split('/');
+            //var listenerContext = arg as HttpListenerContext;
+            var canceller = new CancellationTokenSource();
+            canceller.CancelAfter(maxTimeResponce);
 
+            var url = listenerContext.Request.RawUrl.Split('/');
             var urlParseResult = urlParse(url);
 
             if (urlParseResult == null)
@@ -165,5 +223,6 @@ namespace Kontur.ImageTransformer
         private bool disposed;
         private volatile bool isRunning;
         private int avalibleConnection;
+        public int maxTimeResponce = 1000;
     }
 }
---
+++ Kontur.ImageTransformer/Clipers/BitmapCliper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Kontur.ImageTransformer
+{
+    public class point
+    {
+
+        public point()
+        {
+            blue = 0;
+            alpha = 0;
+            green = 0;
+            red = 0;
+         }
+
+        public byte red { get; set; }
+
+        public byte green { get; set; }
+
+        public byte blue { get; set; }
+
+        public byte alpha { get; set; }
+
+    }
+
+    class BitmapCliper
+    {
+        
+
+        public unsafe static Bitmap Clip(Bitmap img, int startWidth, int startHeight, int newWidth, int newHeight, IFilter filter)
+        {
+            int width = newWidth;
+            int height = newHeight;
+            byte[,,] result = new byte[4, height, width];
+            var a = DateTime.Now;
+            point pixelColorCounter = new point();
+
+            Bitmap resultBitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            BitmapData resultBitmapbytes = resultBitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+
+            BitmapData bd = img.LockBits(new Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            try
+            {
+                byte* curpos;
+                byte* resultBitmapPos;
+                fixed (byte* resultCounter = result)
+                {
+                    byte* alphaCounter = resultCounter;
+                    byte* redCounter = resultCounter + width * height;
+                    byte* greenCounter = resultCounter + 2 * width * height;
+                    byte* blueCounter = resultCounter + 3 * width * height;
+
+                    for (int h = startHeight; h < newHeight; h++)
+                    {
+                        
+                        curpos = ((byte*)bd.Scan0) + h * bd.Stride + startWidth;
+                        resultBitmapPos = ((byte*)resultBitmapbytes.Scan0) + (h - startHeight)*resultBitmapbytes.Stride;
+                        for (int w = startWidth; w < newWidth; w++)
+                        {
+
+                            pixelColorCounter.blue = *(curpos++);
+                            pixelColorCounter.green = *(curpos++);
+                            pixelColorCounter.red = *(curpos++);
+                            pixelColorCounter.alpha = *(curpos++);
+
+                            if (filter != null)
+                                filter.draw(ref pixelColorCounter);
+
+                            *(resultBitmapPos++) = pixelColorCounter.blue; ++blueCounter; 
+                            *(resultBitmapPos++) = pixelColorCounter.green; ++greenCounter;
+                            *(resultBitmapPos++) = pixelColorCounter.red; ++redCounter; 
+                            *(resultBitmapPos++) = pixelColorCounter.alpha; ++alphaCounter;
+
+
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                img.UnlockBits(bd);
+                resultBitmap.UnlockBits(resultBitmapbytes);
+            }
+            var b = DateTime.Now;
+            Console.WriteLine((b - a).Milliseconds);
+            return resultBitmap;
+        }
+        
+        public static void getCorrectCoords(ref Bitmap img, ref int startWidth, ref int startHeight, ref int newWidth, ref int newHeight)
+        {
+            startHeight = Math.Max(0, startHeight);
+            startHeight = Math.Min(startHeight, img.Height);
+
+            startWidth = Math.Max(0, startWidth);
+            startWidth = Math.Min(startWidth, img.Width);
+        }
+    }
+}
